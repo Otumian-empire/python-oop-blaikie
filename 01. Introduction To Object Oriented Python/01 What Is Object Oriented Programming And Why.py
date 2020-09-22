a56d25945219eedf00b7d5c40ee774be7c9dc67c
otumian-empire <popecan1000@gmail.com>
2020-09-22 20:59:00 +0000

    01. Introduction To Object Oriented Python

---
+++ 01. Introduction To Object Oriented Python/01 What Is Object Oriented Programming And Why.py
@@ -0,0 +1,28 @@
+# OOP = paradgim for code organization and design
+# organize functionalities into methods  
+
+# an object: a unit of data that has associated functionality
+# object = data + functionality
+
+# why oop
+# oop organizes code so it is:
+# - easier to use
+# - easier to understand
+# - easier to maintain and extend
+# - easier to collabrate
+# complexity must always  be managed
+# OOP is a universal paradgim
+# It is a necessary step into larger world of SE
+
+# 3 pillars of OOP
+# - encalpsulation
+# - inheritance
+# - polymorphism
+
+# modules vs classes
+# modules = files containing python code
+
+# import module 
+# import module as mod
+# from module import attr, method
+# from module import attr as a, method as m
